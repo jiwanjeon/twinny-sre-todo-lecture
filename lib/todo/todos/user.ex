664823ee25915d51297aad993b1b4301c37ba90e
Jiwan Jeon <jeonjiwan94@gamil.com>
2023-04-27 19:30:23 +0900

    generate todo liveView

---
+++ lib/todo/todos/user.ex
@@ -0,0 +1,19 @@
+defmodule Todo.Todos.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :age, :integer
+    field :email, :string
+    field :name, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:name, :email, :age])
+    |> validate_required([:name, :email, :age])
+  end
+end
